55409bb4feb0179d383be87f84651a60a2289713
Rose Bonner <rose.bonner@reasondigital.com>
2025-12-14 16:01:18 +0000

    styling: update checkbox styling

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -21,17 +21,18 @@ export const RadixCheckbox: React.FC<RadixCheckboxProps> = ({
                 onCheckedChange={(value) => onCheckedChange?.(value === true)}
                 disabled={disabled}
                 className="
-                    flex h-4 w-4 items-center justify-center rounded
+                    flex h-8 w-8 items-center justify-center rounded
                     border border-gray-300 bg-white shadow-sm
                     outline-none
-                    focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500
-                    data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600
-                    disabled:bg-gray-100 disabled:border-gray-200
+                    cursor-pointer
+                    focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600 focus:ring-offset-2
+                    data-[state=checked]:border-black data-[state=checked]:bg-black
+                    disabled:bg-gray-100 disabled:border-gray-200 disabled:pointer-events-none
                 "
             id={id}
         >
             <Checkbox.Indicator className="text-white">
-                <Check className="h-3 w-3" />
+                <Check className="h-5 w-5" />
             </Checkbox.Indicator>
         </Checkbox.Root>
     );
